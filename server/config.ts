7f985813cac10edb1d0d3928102072994696b559
notedit <notedit@gmail.com>
2022-03-21 11:05:46 +0800

    add sdkappid

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,5 @@
 // this sdkappid and secret is for test,  please replace it with your own sdkappid
 export default {
-  sdkappid: 1234567, //1400540319,
-  secret: "xxxxxxxxxxxxx",
+  sdkappid: 1400540319,
+  secret: "5d99bf9896c066b59c907038788fc0706ee822bcd6cd12bb067132c9665ce576",
 };

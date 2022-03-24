035d5766cbb18ab827dbe072d778175a751c257c
cloudwebrtc <duanweiwei1982@gmail.com>
2022-03-25 00:18:57 +0800

    feat: Add flutter sample.

---
+++ flutter/lib/main.dart
@@ -0,0 +1,216 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:random_string/random_string.dart';
+
+import 'wtnclient.dart';
+
+void main() {
+  if (!WebRTC.platformIsWeb) {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+  runApp(WTNSample());
+}
+
+class WTNSample extends StatefulWidget {
+  static String tag = 'whip_publish_sample';
+
+  @override
+  _WTNSampleState createState() => _WTNSampleState();
+}
+
+class _WTNSampleState extends State<WTNSample> {
+  LocalStream? _localStream;
+  final _localRenderer = RTCVideoRenderer();
+  String get stateStr => 'user: $user, stream: $streamId';
+  bool _connecting = false;
+  late WTNClient _wtnClient;
+  final user = randomAlpha(5).toLowerCase();
+  final streamId = randomAlpha(5).toLowerCase();
+
+  TextEditingController _serverController = TextEditingController();
+  TextEditingController _roomController = TextEditingController();
+  late SharedPreferences _preferences;
+
+  @override
+  void initState() {
+    super.initState();
+    initRenderers();
+    _loadSettings();
+  }
+
+  void _loadSettings() async {
+    _preferences = await SharedPreferences.getInstance();
+    this.setState(() {
+      _serverController.text =
+          _preferences.getString('server') ?? 'http://localhost:8000/';
+      _roomController.text = _preferences.getString('room') ?? '123456';
+    });
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    _localRenderer.dispose();
+    _saveSettings();
+  }
+
+  void _saveSettings() {
+    _preferences.setString('server', _serverController.text);
+  }
+
+  void initRenderers() async {
+    await _localRenderer.initialize();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  void _connect() async {
+    final url = _serverController.text;
+    final room = _serverController.text;
+
+    if (url.isEmpty || room.isEmpty) {
+      return;
+    }
+
+    _wtnClient = WTNClient(url);
+
+    _wtnClient.onConnected.stream.listen((event) async {
+      await _wtnClient.join(room, user);
+
+      try {
+        _localStream = LocalStream(streamId);
+        await _localStream!.init(audio: true, video: true);
+
+        setState(() {
+          _localRenderer.srcObject = _localStream!.mediaStream;
+        });
+
+        await _wtnClient.publish(_localStream!);
+      } catch (e) {
+        print('connect: error => ' + e.toString());
+        _localRenderer.srcObject = null;
+        _localStream?.stop();
+        return;
+      }
+    });
+
+    if (!mounted) return;
+
+    setState(() {
+      _connecting = true;
+    });
+  }
+
+  void _disconnect() async {
+    try {
+      _localStream?.stop();
+      _localRenderer.srcObject = null;
+      await _wtnClient.unpublish(_localStream!);
+      setState(() {
+        _connecting = false;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _toggleCamera() async {
+    if (_localStream == null) throw Exception('Stream is not initialized');
+    final videoTrack = _localStream!.mediaStream!
+        .getVideoTracks()
+        .firstWhere((track) => track.kind == 'video');
+    await Helper.switchCamera(videoTrack);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('WTN Sample'), actions: <Widget>[
+        if (_connecting)
+          IconButton(
+            icon: Icon(Icons.switch_video),
+            onPressed: _toggleCamera,
+          ),
+      ]),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Column(children: <Widget>[
+            Column(children: <Widget>[
+              FittedBox(
+                child: Text(
+                  '${stateStr}',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              if (!_connecting)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 18.0, 10.0, 0),
+                  child: Align(
+                    child: Text('Room Server URI:'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              if (!_connecting)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0),
+                  child: TextFormField(
+                    controller: _serverController,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black12)),
+                    ),
+                  ),
+                ),
+              if (!_connecting)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 18.0, 10.0, 0),
+                  child: Align(
+                    child: Text('Room ID:'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              if (!_connecting)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0),
+                  child: TextFormField(
+                    controller: _roomController,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black12)),
+                    ),
+                  ),
+                )
+            ]),
+            if (_connecting)
+              Center(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 110,
+                  decoration: BoxDecoration(color: Colors.black54),
+                  child: RTCVideoView(_localRenderer,
+                      mirror: true,
+                      objectFit:
+                          RTCVideoViewObjectFit.RTCVideoViewObjectFitCover),
+                ),
+              )
+          ]);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _connecting ? _disconnect : _connect,
+        tooltip: _connecting ? 'Hangup' : 'Call',
+        child: Icon(_connecting ? Icons.call_end : Icons.phone),
+      ),
+    ));
+  }
+}
---
+++ flutter/lib/wtnclient.dart
@@ -0,0 +1,243 @@
+import 'dart:async';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:flutter_whip/flutter_whip.dart';
+
+final WTNBaseURL = "https://signaling.rtc.qcloud.com";
+
+class LocalStream {
+  String streamId;
+  MediaStream? mediaStream;
+  String? pushUrl;
+  WHIP? whip;
+  LocalStream(this.streamId);
+
+  Future<void> init({bool? audio, bool? video}) async {
+    mediaStream = await navigator.mediaDevices.getUserMedia({
+      'audio': audio ?? true,
+      'video': video ??
+          {
+            'mandatory': {
+              'minWidth': '1280',
+              'minHeight': '720',
+              'minFrameRate': '30',
+            },
+            'facingMode': 'user',
+            'optional': [],
+          },
+    });
+  }
+
+  Future<void> publish(String url) async {
+    pushUrl = url;
+
+    if (mediaStream == null) {
+      print('mediaStream is null');
+      return;
+    }
+    whip ??= WHIP(url: pushUrl!);
+
+    await whip!.initlize(
+        mode: WhipMode.kSend, stream: mediaStream!, videoCodec: 'h264');
+    await whip!.connect();
+  }
+
+  Future<void> unpublish() async {
+    whip?.close();
+  }
+
+  Future<void> stop() async {
+    mediaStream?.getTracks().forEach((track) async {
+      await track.stop();
+    });
+  }
+}
+
+class RemoteStream {
+  MediaStream? mediaStream;
+  Map<String, dynamic> options;
+  String? playUrl;
+  late String streamId;
+  late String user;
+  WHIP? whip;
+  RemoteStream(this.options) {
+    streamId = options['stream'] as String;
+    user = options['user'] as String;
+  }
+
+  Future<void> init() async {}
+
+  Future<void> subscribe(String playUrl) async {
+    this.playUrl = playUrl;
+    whip ??= WHIP(url: playUrl);
+    whip?.onTrack = (RTCTrackEvent event) {
+      mediaStream = event.streams.first;
+    };
+    await whip!.initlize(mode: WhipMode.kReceive);
+    await whip!.connect();
+  }
+
+  Future<void> unsubscribe() async {
+    whip?.close();
+  }
+
+  Future<void> stop() async {
+    mediaStream?.getTracks().forEach((track) async {
+      await track.stop();
+    });
+  }
+}
+
+class WTNClient {
+  String url;
+  bool connected = false;
+  late IO.Socket socket;
+  String? localUserSig;
+  String? room;
+  String? user;
+  int? sdkappid;
+  LocalStream? localStream;
+  Map<String, RemoteStream> remoteStreams = {};
+  bool joined = false;
+
+  final StreamController<bool> onConnected = StreamController.broadcast();
+  final StreamController<bool> onDisconnected = StreamController.broadcast();
+  final StreamController<dynamic> onUserJoin = StreamController.broadcast();
+  final StreamController<dynamic> onUserLeft = StreamController.broadcast();
+  final StreamController<RemoteStream> onStreamPublished =
+      StreamController.broadcast();
+  final StreamController<RemoteStream> onStreamUnPublished =
+      StreamController.broadcast();
+
+  WTNClient(this.url) {
+    socket = IO.io(
+        url,
+        IO.OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM
+            .enableAutoConnect() // disable auto-connection
+            .setReconnectionDelay(1000)
+            .setReconnectionDelayMax(5000)
+            .build());
+
+    socket.onConnect((_) {
+      print('connect');
+      connected = true;
+      onConnected.add(connected);
+    });
+
+    socket.onDisconnect((_) {
+      print('disconnect');
+      connected = false;
+      onDisconnected.add(connected);
+    });
+
+    socket.on('user-joined', (data) {
+      print('user-joined: $data');
+      onUserJoin.add(data);
+    });
+
+    socket.on('user-left', (data) {
+      print('user-left: $data');
+      final remoteStream = remoteStreams.remove(data['user']);
+      if (remoteStream != null) {
+        remoteStream.stop();
+        onStreamUnPublished.add(remoteStream);
+      }
+      onUserLeft.add(data);
+    });
+
+    socket.on('stream-published', (data) {
+      print('stream-published: $data');
+      final remoteStream = RemoteStream(data);
+      remoteStreams[data['user']] = remoteStream;
+      onStreamPublished.add(remoteStream);
+    });
+
+    socket.on('stream-unpublished', (data) {
+      final remoteStream = remoteStreams.remove(data['user']);
+      if (remoteStream != null) {
+        remoteStream.stop();
+        onStreamUnPublished.add(remoteStream);
+      }
+    });
+
+    socket.connect();
+  }
+
+  Future<dynamic> join(String room, String user) async {
+    this.room = room;
+    this.user = user;
+    Completer<dynamic> completer = Completer<dynamic>();
+    socket.emitWithAck("join", {
+      'room': room,
+      'user': user,
+    }, ack: (Map<String, dynamic> data) {
+      if (data['code'] == 0) {
+        localUserSig = data['userSig'] as String;
+        sdkappid = data['sdkappid'] as int;
+        print("joined, streams ${data['streams']}");
+        data['streams'].forEach((stream) {
+          final remoteStream = RemoteStream(stream);
+          remoteStreams[stream['user']] = remoteStream;
+          onStreamPublished.add(remoteStream);
+        });
+        completer.complete(data);
+      } else {
+        completer.completeError(data);
+      }
+    });
+    return completer.future;
+  }
+
+  Future<dynamic> publish(LocalStream localStream) async {
+    final pushUrl =
+        '$WTNBaseURL/v1/push/${localStream.streamId}?sdkappid=$sdkappid&userid=$user&usersig=$localUserSig';
+    print('pushUrl $pushUrl');
+    await localStream.publish(pushUrl);
+
+    Completer<dynamic> completer = Completer<dynamic>();
+    socket.emitWithAck("publish", {
+      'room': room,
+      'user': user,
+      'stream': localStream.streamId,
+    }, ack: (Map<String, dynamic> data) {
+      if (data['code'] == 0) {
+        print("published ${data}");
+        completer.complete(data);
+      } else {
+        completer.completeError(data);
+      }
+    });
+    return completer.future;
+  }
+
+  Future<void> unpublish(LocalStream localStream) async {
+    await localStream.unpublish();
+    Completer<dynamic> completer = Completer<dynamic>();
+    socket.emitWithAck("unpublish", {
+      'room': room,
+      'user': user,
+      'stream': localStream.streamId,
+    }, ack: (Map<String, dynamic> data) {
+      if (data['code'] == 0) {
+        print("unpublished ${data}");
+        completer.complete(data);
+      } else {
+        completer.completeError(data);
+      }
+    });
+    return completer.future;
+  }
+
+  Future<void> subscribe(RemoteStream remoteStream) async {
+    final playUrl =
+        '$WTNBaseURL/v1/play/${remoteStream.streamId}?sdkappid=$sdkappid&userid=$user&usersig=$localUserSig';
+    print('playUrl $playUrl');
+    await remoteStream.subscribe(playUrl);
+  }
+
+  Future<void> unsubscribe(RemoteStream remoteStream) async {
+    await remoteStream.unsubscribe();
+  }
+}
